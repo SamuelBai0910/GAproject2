86dfa763974d2224454be07ce73dbbdb7d3dae7e
SamuelBai0910 <baisimo2022@163.com>
2023-07-31 11:21:02 +1000

    update header and product index page

--- controllers/products.js
+++ controllers/products.js
@@ -1,11 +1,19 @@
+// controllers/produsts.js
 const Product = require('../models/product');
 
 module.exports = {
   // 其他控制器方法...
-  getFormattedListingDate
+  getFormattedListingDate,
+  index,
 };
+
+async function index(req, res) {
+  const products = await Product.find({});
+  res.render('products/index', { products });
+}
 
 function getFormattedListingDate(listingDate) {
   const options = { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit' };
   return listingDate.toLocaleString('en-US', options);
 }
+
--- routes/products.js
+++ routes/products.js
@@ -1,10 +1,9 @@
 // /routes.js/users.js
 var express = require('express');
 var router = express.Router();
+var productControllers = require('../controllers/products');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+/* GET products */
+router.get('/', productControllers.index);
 
 module.exports = router;
